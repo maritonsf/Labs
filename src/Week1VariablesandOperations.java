4b6978769b666467fb2c1659329618aab9e55351
maritonsf <mari.tonsfeldt@yahoo.com>
2022-07-06 09:09:12 -0500

    first commit

---
+++ src/Week1VariablesandOperations.java
@@ -0,0 +1,23 @@
+
+public class Week1VariablesandOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int planeSeatsAvailable = 7;
+		double groceryCost = 58.23;
+		char middleInitial = 'L';
+		boolean hotOutside = true;
+		String customerName = "John";
+		String address = "779 4th St S";
+		
+		System.out.println(planeSeatsAvailable);
+		System.out.println(groceryCost);
+		System.out.println(middleInitial);
+		System.out.println(hotOutside);
+		System.out.println(customerName);
+		System.out.println(address);
+		
+
+	}
+
+}
